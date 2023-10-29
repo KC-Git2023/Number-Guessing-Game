1741cc7cb53db81e9d86184bb513733d2876f687
KC <cheung.kfj@gmail.com>
2023-10-29 20:09:33 +0800

    feat: Add range validation function and modify UserInterface class
    Reason:
    
    The UserInterface class is responsible for interacting with the user,
    so it has input range validation, which is more reasonable
    than that of the gameFlowController class.
    
    Some methods that only display messages are redundant and should be combined with their corresponding input-getting methods.
    
    Modification:
    
    Remove showMenu()
    
    Remove printRequestInputGuessMessage()
    
    Remove printContinueMessage()
    
    The following methods invoke the getIntInput() instead of using a scanner
    
    getMenuInput()
    getUserGuess()
    getContinueOption()
    
    Edit UserInterface class test
    
    Create integer field variable :
    
    startOfMenuOption,
    
    endOfMenuOption,
    
    startOfContinueOption,
    endOfContinueOption
    
    Create getIntInputWithinRangeOfOption but not yet tested
    
    Reminder:
    
    test getIntInputWithinRangeOfOption
    
    The following methods are awaiting to be edited:
    public int getMenuInput()
    public int getUserGuess()
    public int getContinueOption()

--- source/UserInterface.java
+++ source/UserInterface.java
@@ -3,27 +3,62 @@ import java.util.InputMismatchException;
 
 public class UserInterface {
 	Scanner sc;
+	int startOfMenuOption, endOfMenuOption, startOfContinueOption,
+			endOfContinueOption;
 	
 	public UserInterface() {
 		sc = new Scanner(System.in);
+		
+		startOfMenuOption = 1;
+		endOfMenuOption = 2;
+		
+		startOfContinueOption = 1;
+		endOfContinueOption = 2;
 	}
 	
-	public void showMenu() {
-		System.out.println("1.Start Game");
+	private int getIntInput() {
+		int num;
+		System.out.print("Please Input your option: ");
+		do {
+				try {
+					num = sc.nextInt();
+				break;
+			} catch(InputMismatchException ex){
+					System.out.println("Please input integer");
+					sc.nextLine();					
+			}
+		} while(true);
+		
+		return num;
+  }
+	
+	private int getIntInputWithinRangeOfOption(int startOfRange, int endOfRange) 
+	{
+		int num;
+		do {
+			System.out.printf("Please Input your option between %d and %d\n", startOfRange, endOfRange);
+			
+			num = getIntInput();
+			
+		} while((num < startOfRange) || (num > endOfRange));
+		
+		return num;
+		
+	}
+	
+	public int getMenuInput() {
+	  System.out.println("1.Start Game");
 		System.out.println("2.Exit");
-	}
-	
-	public int getMenuInput() throws InputMismatchException{
-		var userMenuInput = sc.nextInt();
+		
+		var userMenuInput = getIntInput();
 		return userMenuInput;
 	}
 	
-	public void printRequestInputGuessMessage() {
-		System.out.println("Please input a guess");
-	}
 	
-	public int getUserGuess() throws InputMismatchException{
-		var userGuess = sc.nextInt();
+	public int getUserGuess() {
+	  System.out.println("Please input a guess");
+	    
+		var userGuess = getIntInput();
 		return userGuess;
 	}
 	
@@ -49,44 +84,28 @@ public class UserInterface {
 			System.out.println("Unfortunately, you didn't guess it.");
 	}
 	
-	public void printContinueMessage() {
-		System.out.println("Continue?");
-	}
 	
-	public int getContinueOption() {
-		var continueOption = sc.nextInt();
+	public int getUserContinueOption() {
+		System.out.println("Continue?");
+		System.out.println("1.Yes");
+		System.out.println("2.No");
+	    
+		var continueOption = getIntInput();
 		return continueOption;
 	}
 	
-	public int getIntInput() {
-		int num;
-		System.out.println("Please input your option");
-		
-		do {
-				try {
-				num = sc.nextInt();
-				break;
-			} catch(InputMismatchException ex){
-					System.out.println("Please input integer");
-							sc.nextLine();
-					
-			}
-		} while(true);
-		
-		return num;
-   }
+	
+	
 	
 	public static void main(String[] args) {
 		var ui = new UserInterface();
 		int menuInput, userGuess, continueOption;
 		
-		ui.showMenu();
 		menuInput = ui.getMenuInput();
 		System.out.println("User menu input is " + menuInput);
 		
 		System.out.println();
 		
-		ui.printRequestInputGuessMessage();
 		userGuess = ui.getUserGuess();
 		System.out.println("User guess is " + userGuess);
 		
@@ -109,8 +128,7 @@ public class UserInterface {
 		ui.displayResult(true, 3);
 		System.out.println();
 		
-		ui.printContinueMessage();
-		continueOption = ui.getContinueOption();
+		continueOption = ui.getUserContinueOption();
 		System.out.println("User continue Option is " + continueOption);
 		
 		
